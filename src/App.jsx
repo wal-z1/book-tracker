32eb4475d0469e9a90c2ab4b0e14f0722fc4d419
Walid <ow_bouhenika@esi.dz>
2025-08-19 16:36:34 +0100

    some struct

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 function App() {
 	return (
 		<>
-			<h1 class="text-3xl font-bold underline font-mozilla">Hello world!</h1>
+			<h1 class="text-3xl font-bold underline font-mozilla">OH HELLO THERE</h1>
 		</>
 	);
 }
