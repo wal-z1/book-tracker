17c444068fb292155f7a82afd3ac4daa1e5ad3f1
Walid <ow_bouhenika@esi.dz>
2025-08-19 15:11:41 +0100

    mood-tracker

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,4 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-
 function App() {
-	const [count, setCount] = useState(0);
-
 	return (
 		<>
 			<h1 class="text-3xl font-bold underline font-mozilla">Hello world!</h1>
