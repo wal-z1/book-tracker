709527abddea3361aa6f89fbefd891a62532eb31
Walid <ow_bouhenika@esi.dz>
2025-08-20 12:22:04 +0100

    better back drop

--- src/components/MainPage/Header.jsx
+++ src/components/MainPage/Header.jsx
@@ -9,18 +9,16 @@ function Header() {
 
            w-[80%]
          
-          bg-gradient-to-t from-[hsla(146,13%,41%,0.2)] to-[hsla(148,39%,30%,0.4)]
+          bg-gradient-to-b from-[hsla(158,30%,85%,0.3)] to-[hsla(158,30%,75%,0.2)]
 
-         
-          backdrop-blur-sm backdrop-saturate-150
+          backdrop-blur-md backdrop-saturate-150
 
           
-          border-b border-b-[#55806ba8]
-                    border-t border-t-[#194d3b71]
+          border border-[#b8d8cd66]
 
           rounded-md
           
-          shadow-lg shadow-black/30
+          shadow-lg shadow-[#a8c8bd]/40
         "></div>
 		</>
 	);
