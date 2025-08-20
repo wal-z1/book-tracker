42444c9a0dc300cfc275080dc8cd9e52d8ec194e
Walid <ow_bouhenika@esi.dz>
2025-08-20 11:42:55 +0100

    see ya later

--- src/components/MainPage/Header.jsx
+++ src/components/MainPage/Header.jsx
@@ -7,19 +7,20 @@ function Header() {
            mx-auto
            sticky top-5 h-12
 
-           w-[90%]
+           w-[80%]
          
-          bg-gradient-to-t from-[hsla(153,27%,10%,0.2)] to-[hsla(148,39%,20%,0.2)]
+          bg-gradient-to-t from-[hsla(146,13%,41%,0.2)] to-[hsla(148,39%,30%,0.4)]
 
          
           backdrop-blur-sm backdrop-saturate-150
 
           
-          border-b border-y-[#55806ba8]
+          border-b border-b-[#55806ba8]
+                    border-t border-t-[#194d3b71]
 
           rounded-md
           
-          shadow-lg shadow-black/20
+          shadow-lg shadow-black/30
         "></div>
 		</>
 	);
