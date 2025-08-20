1915b0e2c86e47f40444ff079547c8e14d9168f1
Walid <ow_bouhenika@esi.dz>
2025-08-20 10:14:41 +0100

    let's design  the header

--- src/components/MainPage/Header.jsx
+++ src/components/MainPage/Header.jsx
@@ -2,7 +2,7 @@ function Header() {
 	return (
 		<>
 			<div
-				className=" min-w-full font-inter font-black sticky h-10 
+				className=" min-w-full font-bold sticky h-10 
     bg-gradient-to-tr from-[#86EFAC] to-[#22C582]
   ">
 				SOME TEXT FOR NOW
