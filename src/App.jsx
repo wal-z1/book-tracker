c56011814e301f4904ffd965c065ba23ded5ff79
Walid <ow_bouhenika@esi.dz>
2025-08-21 14:17:48 +0100

    later loosers

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Header from "./components/MainPage/Header";
 import OrganicBackground from "./components/OrganicBackground";
 import Footer from "./components/MainPage/Footer";
+import Home from "./components/MainPage/Home";
 function App() {
 	return (
 		<>
 			<OrganicBackground />
 			<Header />
+			<Home/>
 			<Footer />
 		</>
 	);
