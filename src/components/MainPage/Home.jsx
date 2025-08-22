242e5b0c0c6f1e4edbbc39472d1e3f4cf8afb5eb
Walid <ow_bouhenika@esi.dz>
2025-08-22 11:47:58 +0100

    UPDATE INTO AN OBJECT

--- src/components/MainPage/Home.jsx
+++ src/components/MainPage/Home.jsx
@@ -2,10 +2,10 @@ import AddBook from "../Util/AddBook";
 import CurrentlyState from "../Util/CurrentlyState";
 function Home() {
 	return (
-		<>
+		<div className="flex flex-col gap-10">
 			<AddBook />
-      <CurrentlyState/>
-		</>
+			<CurrentlyState />
+		</div>
 	);
 }
 export default Home;
--- src/components/Util/CurrentlyState.jsx
+++ src/components/Util/CurrentlyState.jsx
@@ -2,20 +2,18 @@
 
   function CurrentlyState() {
     const [CurrentBook, UpdateBook] = useState("No Book");
-    const [CurrentPage, UpdatePage] = useState("0");
-    const [AllPage, UpdateAllPage] = useState("undefined ");
+    const [CurrentPage, UpdatePage] = useState(0);
+    const [AllPage, UpdateAllPage] = useState(undefined);
 
     return (
       <div
         className="container   font-inter capitalize font-medium 
-          
-          text-shadow-blue-500 
-          cursor-pointer w-fit
-          text-sm
-          mx-auto
-          relative 
-          top-12">
-        <span className="text-[#8B949E]">
+            w-fit
+            text-md
+            mx-auto
+            relative 
+            top-12">
+        <span className="text-[#8B949E]  [text-shadow:0_0_10px_rgba(88,166,255,0.6),_1px_1px_1px_rgba(10,10,10)] ">
           {" "}
           Currently Reading: "{CurrentBook}" (Page {CurrentPage}/{AllPage}){" "}
         </span>
