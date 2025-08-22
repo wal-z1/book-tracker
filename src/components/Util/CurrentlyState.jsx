b4d32001bea468339c3a02d1cd1cb018244aeff1
Walid <ow_bouhenika@esi.dz>
2025-08-22 13:13:45 +0100

    SURE

--- src/components/Util/CurrentlyState.jsx
+++ src/components/Util/CurrentlyState.jsx
@@ -2,7 +2,7 @@ function CurrentlyState({ book }) {
 	return (
 		<div
 			className="container font-inter capitalize font-medium 
-            w-fit text-sm sm:text-md mx-auto relative top-8 sm:top-12 px-2 text-center sm:text-left">
+            w-fit text-md sm:text-lg mx-auto relative top-8 sm:top-12 px-2 text-center sm:text-left">
 			<span className="text-[#8B949E] [text-shadow:0_0_10px_rgba(88,166,255,0.6),_1px_1px_1px_rgba(10,10,10)]">
 				Currently Reading: "{book.title}" (Page {book.currentPage}/
 				{book.totalPages})
--- src/components/Util/Twogrid.jsx
+++ src/components/Util/Twogrid.jsx
@@ -1,7 +1,7 @@
 function Twogrid() {
 	return (
 		<div className="grid grid-cols-1 sm:grid-cols-3 p-6 sm:p-10 gap-4 text-center mx-auto">
-			<div className="text-[#F0F6FC] text-lg sm:text-2xl font-outfit [text-shadow:0_0_10px_rgba(88,166,255,0.6),_1px_1px_1px_rgba(10,10,10)]">
+			<div className="text-[#F0F6FC] text-lg sm:text-2xl font-outfit [text-shadow:0_0_10px_rgba(88,166,255,0.6),_1px_1px_1px_rgba(10,10,10)] cursor-pointer">
 				[ 📖 View Library ]
 			</div>
 
@@ -9,7 +9,7 @@ function Twogrid() {
 				|
 			</div>
 
-			<div className="text-[#F0F6FC] text-lg sm:text-2xl font-outfit [text-shadow:0_0_10px_rgba(88,166,255,0.6),_1px_1px_1px_rgba(10,10,10)]">
+			<div className="text-[#F0F6FC] text-lg sm:text-2xl font-outfit [text-shadow:0_0_10px_rgba(88,166,255,0.6),_1px_1px_1px_rgba(10,10,10)]   cursor-pointer ">
 				[ 📊 Check Stats ]
 			</div>
 		</div>
