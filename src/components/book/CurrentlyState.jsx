a96e3241e2a37f7658c7de3f9590acafa5ebc28f
Walid <ow_bouhenika@esi.dz>
2025-08-23 10:57:12 +0100

    zzz

--- src/components/book/CurrentlyState.jsx
+++ src/components/book/CurrentlyState.jsx
@@ -3,7 +3,7 @@ function CurrentlyState({ book }) {
 		<div
 			className="container font-inter capitalize font-medium 
             w-fit text-md sm:text-lg mx-auto relative top-8 sm:top-12 px-2 text-center sm:text-left">
-			<span className="text-[#8B949E] [text-shadow:0_0_10px_rgba(88,166,255,0.6),_1px_1px_1px_rgba(10,10,10)]">
+			<span className=" cursor-pointer text-[#8B949E] [text-shadow:0_0_10px_rgba(88,166,255,0.6),_1px_1px_1px_rgba(10,10,10)]">
 				Currently Reading: "{book.title}" (Page {book.currentPage}/
 				{book.totalPages})
 			</span>
