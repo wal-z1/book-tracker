3807fb46a027f86538cb0ef8d2596b0c04077e12
Walid <ow_bouhenika@esi.dz>
2025-08-23 09:28:20 +0100

    better folder structure

---
+++ src/components/book/Popcard.jsx
@@ -0,0 +1,20 @@
+import { motion, AnimatePresence } from "framer-motion";
+
+function PopCard({ children, open }) {
+	return (
+		<div
+			className={`
+        bg-[#1a2638] 
+        rounded-2xl 
+        shadow-lg shadow-black/40 
+        p-6 
+        w-[400px] max-w-[90%]
+        text-[#F0F6FC]
+        relative
+      `}>
+			{children}
+		</div>
+	);
+}
+
+export default PopCard;
