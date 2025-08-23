2d3fe962590e80c0d76bfe80f3007cd067b9bf10
Walid <ow_bouhenika@esi.dz>
2025-08-23 12:47:23 +0100

    push

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -17,11 +17,11 @@ function Header() {
 			}}
 			whileHover={{ backgroundColor: "rgba(0,0,0,0.15)" }}
 			className="
-        p-2 sm:p-3 sm:px-6 flex flex-wrap items-center justify-center sm:justify-evenly mx-auto sticky z-50
+       flex flex-wrap items-center justify-center sm:justify-evenly mx-auto sticky z-50
         min-h-12 w-screen rounded-md
         bg-gradient-to-b from-[#131823]/30 to-[#131823]/20
         backdrop-blur-lg backdrop-saturate-150
-        border border-[#313B4A]/40 
+        border border-t-[#313B4A]/40 
         shadow-lg shadow-[#58A6FF]/10
       ">
 			<NavBar />
