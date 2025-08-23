130058d807b929a84b2ff20683ce8ea3d08f9f1c
Walid <ow_bouhenika@esi.dz>
2025-08-23 12:37:21 +0100

    saving

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Header from "./components/layout/Header";
 import OrganicBackground from "./components/layout/OrganicBackground";
 import Footer from "./components/layout/Footer";
 import Home from "./pages/HomePage";
+import { Analytics } from "@vercel/analytics/react";
 function App() {
 	return (
 		<>
@@ -9,6 +10,7 @@ function App() {
 			<Header />
 			<Home />
 			<Footer />
+			<Analytics />
 		</>
 	);
 }
--- src/components/book/BookMenu.jsx
+++ src/components/book/BookMenu.jsx
@@ -1,4 +1,19 @@
 export default function BookMenu({ setBook, SetLibrary }) {
+	const [title, setTitle] = useState("");
+	const [pages, setPages] = useState("");
+	const [author, setAuthor] = useState("");
+	const Save = () => {
+		const newBook = {
+			title,
+			author,
+			currentPage: 0,
+			totalPages: Number(pages),
+		};
+
+		setBook(newBook); // update current book
+		SetLibrary((prev) => [...prev, newBook]); // add to library
+	};
+
 	const input =
 		"block w-full p-2.5 text-sm text-gray-200 placeholder-gray-400 bg-gray-800 border border-gray-600 rounded-lg transition-colors duration-200 ease-in-out focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent";
 	return (
@@ -7,16 +22,28 @@ export default function BookMenu({ setBook, SetLibrary }) {
 				add a new book
 			</span>
 			<div className="mt-3 container flex flex-col gap-5 items-center justify-center">
-				<input className={input} placeholder="Book title..." />
+				<input
+					className={input}
+					value={title}
+					placeholder="Book title..."
+					onChange={(e) => setTitle(e.target.value)}
+				/>
 				<input
 					className={input}
 					min={1}
+					value={pages}
 					type="number"
 					placeholder="Number Of Pages..."
+					onChange={(e) => setPages(e.target.value)}
 				/>
-				<input className={input} placeholder="Author..." />
-				<button 
-				onClick={()=>setBook}
+				<input
+					className={input}
+					value={author}
+					placeholder="Author..."
+					onChange={(e) => setAuthor(e.target.value)}
+				/>
+				<button
+					onClick={() => Save()}
 					className="font-inter text-sm sm:text-base capitalize font-medium
         px-3 sm:px-4 py-1.5 sm:py-2 bg-[#131823]
         rounded-md border border-[#3B6A99]
