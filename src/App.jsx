8c0672918b9c4a41728def1a7be89121dcf4ba71
Walid <ow_bouhenika@esi.dz>
2025-08-24 09:09:29 +0100

    creating pages

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,11 @@ import Header from "./components/layout/Header";
 import OrganicBackground from "./components/layout/OrganicBackground";
 import Footer from "./components/layout/Footer";
 import Home from "./pages/HomePage";
+import Library from "./pages/Library";
+import Stats from "./pages/Stats";
 import { Analytics } from "@vercel/analytics/react";
+import { BrowserRouter } from "react-router-dom";
+
 function App() {
 	return (
 		<>
